939a2fa1641d9d3f3e9c2e4f1a141cd182784c26
Peter Qian <peter.qian@clario.com>
2023-11-28 09:15:16 +0100

    Replace ngFor with @for

--- src/app/signals/signals.component.ts
+++ src/app/signals/signals.component.ts
@@ -1,11 +1,11 @@
-import { NgFor } from '@angular/common';
-import { Component, signal, computed, effect } from '@angular/core';
+import { NgFor } from "@angular/common";
+import { Component, signal, computed, effect } from "@angular/core";
 
 @Component({
-  selector: 'app-signals',
-  templateUrl: './signals.component.html',
+  selector: "app-signals",
+  templateUrl: "./signals.component.html",
   standalone: true,
-  imports: [NgFor]
+  imports: [NgFor],
 })
 export class SignalsComponent {
   actions = signal<string[]>([]);
@@ -13,24 +13,24 @@ export class SignalsComponent {
   doubleCounter = computed(() => this.counter() * 2);
 
   constructor() {
-    effect(() => console.log('Counter: ', this.counter()));
+    effect(() => console.log("Counter: ", this.counter()));
   }
 
   increment() {
     this.counter.update((oldCounter) => oldCounter + 1);
     // this.actions.push('INCREMENT');
-    this.actions.mutate((oldActions) => oldActions.push('INCREMENT'));
+    this.actions.update((oldActions) => [...oldActions, "INCREMENT"]);
   }
 
   decrement() {
     this.counter.update((oldCounter) => oldCounter - 1);
     // this.actions.push('DECREMENT');
-    this.actions.update((oldActions) => [...oldActions, 'DECREMENT']);
+    this.actions.update((oldActions) => [...oldActions, "DECREMENT"]);
   }
 
   set() {
     this.counter.set(5);
     // this.actions.push('SET');
-    this.actions.mutate((oldActions) => oldActions.push('SET'));
+    this.actions.update((oldActions) => [...oldActions, "SET"]);
   }
 }
